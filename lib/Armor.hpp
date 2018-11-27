b5065a45534334f9d6af6b08735a6a2c8ab857a7
Alejandro Perez <alex2721ap@gmail.com>
2018-11-27 15:45:56 -0800

    Armor now works

--- lib/Armor.hpp
+++ lib/Armor.hpp
@@ -1,11 +1,11 @@
 #pragma once
-
 #include <cstdlib>
+
 class Armor
 {
 private:
     int defense;
 public:
     Armor(): defense(5) {} //base value for armor defense
-    virtual int getDefense()const;
+    virtual int getDefense()const{return defense;}
 };
