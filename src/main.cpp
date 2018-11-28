fa8bc143e94482dded8199c932dfe0dbd3a417b6
Alejandro Perez <alex2721ap@gmail.com>
2018-11-27 16:46:06 -0800

    Metal and Leather Armor work

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include "../lib/Armor.hpp"
 using namespace std;
 
 int main() {
 
-    Armor armor;
-
-    cout << "Base defense is " << armor.getDefense() << endl;
 
     cout << "Weapon and Potion is an Decorator" << endl;
 
