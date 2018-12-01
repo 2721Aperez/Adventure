2a2cb1e6ce4c371a95f20a93aad763415520f13e
Alejandro Perez <alex2721ap@gmail.com>
2018-11-30 16:10:14 -0800

    Added Changes

--- lib/Player.hpp
+++ lib/Player.hpp
@@ -3,12 +3,7 @@
 #include <stdlib.h>
 #include <cmath.h>
 #include "Direction.hpp"
-<<<<<<< HEAD
 #inlcude "Enemy.hpp"
-=======
-#include "Inventory.hpp"
-
->>>>>>> 5f18159f51c71d4f9f51ca6653acf0e9020e78c3
 
 class Player
 {
@@ -19,21 +14,11 @@ private:
 
 public:
     Player();
-<<<<<<< HEAD
     int getHP() const;
     int getAttackPower() const;
     void setHP(int hp);
     void takeDamage(Enemy *enemy);
-    void attack(Enemy *enemy);
     void dodge();
     bool isDead();
     ~Player();
 };
-=======
-    void attack();
-    void pickUp();
-    void drop();
-    void useItem();//Item type in parameter
-    void move(Direction);//Direction type in parameter
-};
->>>>>>> 5f18159f51c71d4f9f51ca6653acf0e9020e78c3
