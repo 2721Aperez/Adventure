64d96a96f56f333e1f8c012b86797eaf8bdf8108
Alejandro Perez <alex2721ap@gmail.com>
2018-12-02 18:24:57 -0800

    Check Your inventory

--- lib/Inventory.cpp
+++ lib/Inventory.cpp
@@ -1,51 +1,69 @@
 #include "Inventory.hpp"
 
-void Inventory::addWeapon(Weapon *weaponType)
+void Inventory::addWeapon(Weapon *weaponType) 
 {
-    int remaining_weight = max_weight - current_weight;
+	int remaining_weight = max_weight - current_weight;
 
-    if(weaponType->getWeight()  >= max_weight)
-    {
-        std::cout << "You don't have enough room for this weapon." << std::endl;
-    }
-    else if(remaining_weight < weaponType->getWeight())
-    {
-        std::cout << "You have no more room for this weapon.  Drop an Item or something you hoarder." << std::endl;
-    }
-    else
-    { 
-        weapon = weaponType;
-        current_weight += weaponType->getWeight();
-    }
+	if (weaponType->getWeight() >= max_weight)
+	{
+		std::cout << "You don't have enough room for this weapon." << std::endl;
+	}
+	else if (remaining_weight < weaponType->getWeight())
+	{
+		std::cout << "You have no more room for this weapon.  Drop an Item or something you hoarder." << std::endl;
+	}
+	else
+	{
+		weapon = weaponType;
+		current_weight += weaponType->getWeight();
+	}
 }
 
 void Inventory::addPotion(Potion *potionType)
 {
-    int remaining_weight = max_weight - current_weight;
+	int remaining_weight = max_weight - current_weight;
 
-    if(potionType->getWeight() >= max_weight)
-    {
-        std:: cout << "You don't have enough room for this potion." << std::endl;
-    }
-    else if(remaining_weight < potionType->getWeight())
-    {
-        std::cout << "You have no more room for this potion. Drop something you hoarder" << std::endl;
-    }
-    else
-    {
-        potion = potionType;
-        current_weight += potionType->getWeight();
-    }
+	if (potionType->getWeight() >= max_weight)
+	{
+		std::cout << "You don't have enough room for this potion." << std::endl;
+	}
+	else if (remaining_weight < potionType->getWeight())
+	{
+		std::cout << "You have no more room for this potion. Drop something you hoarder" << std::endl;
+	}
+	else
+	{
+		potion.push_back(potionType);
+		current_weight += potionType->getWeight();
+	}
 }
 
-void Inventory::removePotion()
+void Inventory::removePotion(int i)
 {
-    delete potion;
+	int j = 0;
+	for (auto it = potion.begin(); it != potion.end(); it++)
+	{
+		j++;
+		if (j == i)
+		{
+			potion.erase(it);
+			break;
+		}
+	}
 }
 
 void Inventory::removeWeapon()
 {
-    delete weapon;
+	delete weapon;
 }
 
-int Inventory::checkCapacity(){ return current_weight;}
+int Inventory::checkCapacity()
+{
+	return current_weight;
+}
+
+void Inventory::checkInventory()
+{
+	if (weapon == nullptr) { std::cout << "You have no weapons." << std::endl; }
+    if (potion.empty()) { std::cout << "You have no potions." << std::endl; }
+}
--- lib/Inventory.hpp
+++ lib/Inventory.hpp
@@ -2,21 +2,25 @@
 #include "Stick.hpp"
 #include "DivineAxeRhitta.hpp"
 #include "Potion.hpp"
+#include "vector"
 
 #pragma once
+
+using std::vector;
 
 class Inventory
 {
 private:
-    int max_weight;
-    int current_weight;
-    Weapon *weapon;
-    Potion *potion;
+	int max_weight;
+	int current_weight;
+	Weapon *weapon;
+	vector<Potion*> potion;
 public:
-    Inventory(): max_weight(50){}
-    void addWeapon(Weapon *weaponType);
-    void addPotion(Potion *potionType);
-    void removePotion();
-    void removeWeapon();
-    int checkCapacity();
+	Inventory() : max_weight(50), weapon(nullptr), potion{nullptr} {}
+	void addWeapon(Weapon *weaponType);
+	void addPotion(Potion *potionType);
+	void removePotion(int i);
+	void removeWeapon();
+	int checkCapacity();
+	void checkInventory();
 };
